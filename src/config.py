e44c8dc96daaa6dc010bc6b1eca5e2b73efa18be
kostya shilkrot <kostya.shilkrot@gmail.com>
2025-08-24 16:43:03 +0300

    rearrange dirs

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DB_NAME: str | None = None
+    DB_NAME: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
