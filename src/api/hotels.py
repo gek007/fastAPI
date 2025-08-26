9b91000a95b2bbde3aed800224a7c5e48d84ed2b
kostya shilkrot <kostya.shilkrot@gmail.com>
2025-08-26 18:25:33 +0300

    added get by id

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -24,6 +24,12 @@ async def get_hotels(
             offset=per_page * (pagination.page - 1)
         )
 
+@router.get("/{hotel_id}")
+async def delete_hotel(hotel_id: int):
+        async with async_session_maker() as session:
+            return await HotelsRepository(session).get_one_or_none(id=hotel_id)
+
+
 @router.delete("/{hotel_id}")
 async def delete_hotel(hotel_id: int):
         async with async_session_maker() as session:
